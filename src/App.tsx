7c0708d7420869c0713aba3c8204f2c2cc491b19
josiasoliveirazup <josias.oliveira@zup.com.br>
2021-07-22 15:27:26 -0300

    React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Background from './Background';
+
+function App() {
+  return (
+    <div className="App">
+      <Background />
+      <h1>Explore repositórios no GitHub</h1>
+    </div>
+  );
+}
+
+export default App;
